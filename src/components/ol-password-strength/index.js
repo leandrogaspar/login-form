76be2a86e55cf1668dea75247e8bf88a8408febb
Leandro Gaspar Scanavini <leandrogaspar789@gmail.com>
2018-10-23 19:03:46 -0300

    Added index.js to improve component import

---
+++ src/components/ol-password-strength/index.js
@@ -0,0 +1,1 @@
+export { default } from './ol-password-strength'
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,8 +1,8 @@
 // Webpack entry point. Just import everything you need to put on the bundle here :)
-import OlLogotype from '../../components/ol-logotype/ol-logotype';
-import OlInputText from '../../components/ol-input-text/ol-input-text';
-import OlPasswordStrength from '../../components/ol-password-strength/ol-password-strength';
-import OlButton from '../../components/ol-button/ol-button';
+import OlLogotype from '../../components/ol-logotype';
+import OlInputText from '../../components/ol-input-text';
+import OlPasswordStrength from '../../components/ol-password-strength';
+import OlButton from '../../components/ol-button';
 
 // Copy the .html to dist
 require('file-loader?name=[name].[ext]!./index.html');
